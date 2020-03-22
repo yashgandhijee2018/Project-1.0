617d3469017acdc0cd3f4eaaf32fde62e4f4776a
HP <vaibhavsrivastava2107@gmail.com>
2020-03-22 18:43:11 +0530

    TextWatcher Added for Auto Focus in Otp Reading

--- app/src/main/java/com/demo/incampus/OTPJava.java
+++ app/src/main/java/com/demo/incampus/OTPJava.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class OTPJava extends AppCompatActivity {
 
@@ -17,6 +20,32 @@ public class OTPJava extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_java);
+
+        //TextWatcher so that the focus automatically moves with the number entered.
+        final StringBuilder sb = new StringBuilder();
+        final EditText editTextOne = findViewById(R.id.editTextone);
+        final EditText editTextTwo = findViewById(R.id.editTexttwo);
+
+        editTextOne.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(sb.length()==0 && editTextOne.length()==1) {
+                    sb.append(s);
+                    editTextOne.clearFocus();
+
+                    editTextTwo.requestFocus();
+                    editTextTwo.setCursorVisible(true);
+
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
     }
