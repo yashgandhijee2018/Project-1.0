5ff19456cd15ced840b398270ba6b5148f3c60de
HP <vaibhavsrivastava2107@gmail.com>
2020-03-24 12:17:05 +0530

    Counter Code Spinner Removed :(

--- app/src/main/java/com/demo/incampus/VerifyNumberActivity.java
+++ app/src/main/java/com/demo/incampus/VerifyNumberActivity.java
@@ -2,37 +2,25 @@ package com.demo.incampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class VerifyNumberActivity extends AppCompatActivity {
 
-    private ArrayList<CountryCodeAdapter.CountryCode> mCode;
-    private CountryCodeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_number);
-
-        codeList();
-
-        Spinner spinner = findViewById(R.id.spinner);
-
-        adapter = new CountryCodeAdapter(this,mCode);
-        spinner.setAdapter(adapter);
-
-        spinner.getOnItemSelectedListener();
     }
 
-    private void codeList() {
-        mCode = new ArrayList<>();
-        mCode.add(new CountryCodeAdapter.CountryCode("+91"));
-        mCode.add(new CountryCodeAdapter.CountryCode("+92"));
-        mCode.add(new CountryCodeAdapter.CountryCode("+93"));
-        mCode.add(new CountryCodeAdapter.CountryCode("+94"));
-        mCode.add(new CountryCodeAdapter.CountryCode("+95"));
-    }
+//    public void verify (View view) {
+//        Intent intent = new Intent();
+//        startActivity(intent);
+//    }
+
 }
